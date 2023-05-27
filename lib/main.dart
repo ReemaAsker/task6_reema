30227a2de94c3474c174a4dfb97106356e261afa
Reema Asker <reema1822000@gmail.com>
2023-05-28 01:15:44 +0300

    changes to be more compitable with task

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import 'package:flutter/services.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromRGBO(18, 5, 56, 5),
         body: paper_rock(),
@@ -22,6 +24,8 @@ class MyApp extends StatelessWidget {
 }
 
 class paper_rock extends StatefulWidget {
+  const paper_rock({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _paper_rock();
@@ -31,9 +35,11 @@ class paper_rock extends StatefulWidget {
 class _paper_rock extends State<StatefulWidget> {
   late int rightimg;
   late int leftimg;
-  bool lwin = false;
-  bool rwin = false;
-  String res = 'Equal';
+
+  List<Icon> leftIcons = [];
+  List<Icon> rightIcons = [];
+  List<Icon> equalIcons = [];
+
 //1  rock
 //2  sciss
 //3  paper
@@ -47,33 +53,112 @@ class _paper_rock extends State<StatefulWidget> {
     setState(() {
       leftimg = math.Random().nextInt(3) + 1;
       rightimg = math.Random().nextInt(3) + 1;
+
       the_win();
     });
   }
 
+  void end_play() {
+    // if (leftIcons.length * 24 >=
+    //         MediaQuery.of(context).size.width.toInt() - 100 ||
+    //     rightIcons.length * 24 >=
+    //         MediaQuery.of(context).size.width.toInt() - 100 ||
+    //     equalIcons.length * 24 >=
+    //         MediaQuery.of(context).size.width.toInt() - 100) {
+    // print('${leftIcons.length} ,${equalIcons.length},${rightIcons.length}');
+    if (leftIcons.length == 12 ||
+        rightIcons.length == 12 ||
+        equalIcons.length == 12) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Center(
+                    child: leftIcons.length != 12
+                        ? const Icon(
+                            Icons.sentiment_very_dissatisfied,
+                            color: Colors.red,
+                            size: 150,
+                          )
+                        : const Icon(
+                            Icons.sentiment_satisfied_alt,
+                            color: Colors.green,
+                            size: 150,
+                          )),
+              ),
+              const Center(
+                child: Text(
+                  'END Game',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Center(
+                child: Text(
+                  'YOUR SCORE IS ${leftIcons.length} /12',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              TextButton(
+                child: Container(
+                  margin: const EdgeInsets.all(45.0),
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.blue),
+                  child: const Center(
+                    child: Text(
+                      'Play Again',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/', (Route<dynamic> route) => false);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   void the_win() {
-    if (leftimg == 1 && rightimg == 2) {
-      lwin = true;
-    } else if (leftimg == 1 && rightimg == 3) {
-      rwin = true;
-    } else if (leftimg == 2 && rightimg == 1) {
-      rwin = true;
-    } else if (leftimg == 2 && rightimg == 3) {
-      lwin = true;
-    } else if (leftimg == 3 && rightimg == 1) {
-      lwin = true;
-    } else if (leftimg == 3 && rightimg == 2) {
-      rwin = true;
-    }
-    if (lwin) {
-      lwin = false;
-      res = 'Left';
-    } else if (rwin) {
-      rwin = false;
-      res = 'Right';
+    end_play();
+
+    if (leftimg == 1 && rightimg == 2 ||
+        leftimg == 2 && rightimg == 3 ||
+        leftimg == 3 && rightimg == 1) {
+      leftIcons.add(Icon(Icons.thumb_up_alt, color: Colors.purple));
+    } else if (leftimg == 1 && rightimg == 3 ||
+        leftimg == 2 && rightimg == 1 ||
+        leftimg == 3 && rightimg == 2) {
+      rightIcons.add(Icon(Icons.thumb_up_alt, color: Colors.teal[700]));
     } else {
-      res = 'Equal';
+      equalIcons.add(Icon(Icons.diamond_sharp, color: Colors.amber));
     }
+    end_play();
   }
 
   @override
@@ -81,14 +166,17 @@ class _paper_rock extends State<StatefulWidget> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        const Image(
-          height: 200,
-          width: 200,
-          image: AssetImage('images/name1.png'),
-          fit: BoxFit.cover,
+        const Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Image(
+            height: 200,
+            width: 200,
+            image: AssetImage('images/name1.png'),
+            fit: BoxFit.cover,
+          ),
         ),
         Container(
-          padding: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(15.0),
           decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('images/backplay.jpg'), fit: BoxFit.cover),
@@ -112,17 +200,40 @@ class _paper_rock extends State<StatefulWidget> {
             ],
           ),
         ),
-        Column(
-          children: [
-            Text(
-              'The win is the',
-              style: TextStyle(color: Colors.white, fontSize: 20.0),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'You   ',
+                style: TextStyle(color: Colors.purple, fontSize: 35.0),
+              ),
+              Text(
+                '      System',
+                style: TextStyle(color: Colors.teal[700], fontSize: 35.0),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                children: leftIcons,
+              ),
             ),
-            Text(
-              '$res',
-              style: TextStyle(color: Colors.red, fontSize: 30.0),
+            Expanded(
+              child: Column(
+                children: equalIcons,
+              ),
             ),
-          ],
+            Expanded(
+              child: Column(
+                children: rightIcons,
+              ),
+            ),
+          ]),
         ),
       ],
     );
